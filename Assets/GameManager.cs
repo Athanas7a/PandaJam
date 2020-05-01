83ca4bc7831ad92c24cbfb0aaf86a87299e7e3f2
artanasia <61665352+artanasia@users.noreply.github.com>
2020-05-01 19:58:11 +0200

    without PS

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private bool _isGameOver;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R) == true && _isGameOver == true)
+        {
+            SceneManager.LoadScene(1); //Current game scene
+        }
+    }
+    public void GameOver()
+    {
+        _isGameOver = true;
     }
 }
