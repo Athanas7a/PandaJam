8a8586da3451344caabd9d23caaacf67eaf603a8
artanasia <61665352+artanasia@users.noreply.github.com>
2020-05-02 02:44:41 +0200

    update terrain +

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+	// store a public reference to the Player game object, so we can refer to it's Transform
+	public GameObject player;
+
+	// Store a Vector3 offset from the player (a distance to place the camera from the player at all times)
+	private Vector3 offset;
+
+	// At the start of the game..
+	void Start()
+	{
+		if (player == null)
+		{
+			Debug.LogWarning("Camera missing target ref !", this);
+
+			return;
+		}
+		
+		// Create an offset by subtracting the Camera's position from the player's position
+		offset = transform.position - player.transform.position;
+	}
+
+	// After the standard 'Update()' loop runs, and just before each frame is rendered..
+	void LateUpdate()
+	{
+		// Set the position of the Camera (the game object this script is attached to)
+		// to the player's position, plus the offset amount
+		transform.position = player.transform.position + offset;
+		transform.LookAt(player.transform, Vector3.up);
+	}
+}
